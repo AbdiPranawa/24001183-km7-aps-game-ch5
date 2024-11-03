400b4b4cc8c96ed8c8ed2939f0e9fc3bfe0cd623
AbdiPranawa <asaturaardana@gmail.com>
2024-11-03 13:25:22 +0700

    update superAdmin model

--- models/superAdmin.js
+++ models/superAdmin.js
@@ -1,48 +1,22 @@
 "use strict";
 const { Model } = require("sequelize");
-module.exports = (sequelize, DataTypes) => {
-  class Users extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      Users.hasOne(models.Auths, { foreignKey: "userId", as: "auth" });
-      Users.hasMany(models.Cars, { foreignKey: "userId", as: "cars" });
-    }
-  }
 
-  Users.init(
+module.exports = (sequelize, DataTypes) => {
+  class Superadmin extends Model {}
+  Superadmin.init(
     {
-      name: {
+      email: {
         type: DataTypes.STRING,
         allowNull: false,
-        validate: {
-          len: [3, 100],
-        },
+        unique: true,
+        validate: { isEmail: true },
       },
-      age: {
-        type: DataTypes.INTEGER,
-        validate: {
-          min: 0,
-        },
-      },
-      address: {
+      password: {
         type: DataTypes.STRING,
-      },
-      role: {
-        type: DataTypes.ENUM("admin", "member"),
         allowNull: false,
-        defaultValue: "member",
       },
     },
-    {
-      sequelize,
-      modelName: "Users",
-    }
+    { sequelize, modelName: "Superadmin" }
   );
-
-  return Users;
+  return Superadmin;
 };
